9bec5fd29a6ace8860571224aca3231a19db1673
gmaclinuxer <gmaclinuxer@github.com>
2017-12-22 17:37:12 +0800

    feature: tutorial-2

--- app.py
+++ app.py
@@ -1,21 +1,33 @@
 # -*- coding: utf-8 -*-
-
-from flask import Flask, url_for
-from flask import request
+import os
+from flask import Flask, url_for, make_response
+from flask import request, render_template
+from flask import redirect, abort
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+
+def debug_request():
+    print request.args, request.form, request.is_json
 
 
 @app.route('/')
 def index():
-    # 1/0
-    return 'index'
+    debug_request()
+    return render_template('index.html', **{
+        'title': u"标题",
+        'name': 'pitou',
+    })
 
 
 # 访问 /about/ 404
 @app.route('/about')
 def about():
-    return 'about'
+    # 1/0
+    return redirect(url_for('index'))
 
 
 # 访问/user/1 重定向到 /user/1/
@@ -24,8 +36,13 @@ def user(username):
     return 'username: %s' % username
 
 
-@app.route('/post/<int:post_id>')
+@app.route('/post/<int:post_id>', methods=['GET', 'POST'])
 def post(post_id):
+    debug_request()
+    f = request.files['the_file']
+    filename = '%s_upload' % secure_filename(f.filename)
+    f.save(os.path.join(BASE_DIR, filename))
+
     return 'post_id: %s' % post_id
 
 
@@ -34,6 +51,17 @@ def login():
     if request.method == 'POST':
         return 'login: post'
     return 'login: get'
+
+
+@app.route('/cookies', methods=['GET', 'POST'])
+def cookies():
+    str = ''
+    for k, v in request.cookies.iteritems():
+        str += '%s: %s | ' % (k, v)
+    resp = make_response('cookies: \n%s' % str)
+
+    resp.set_cookie('hello', 'world')
+    return resp
 
 
 # 代码自动重载，500错误时支持在页面调试 Werkzeug
