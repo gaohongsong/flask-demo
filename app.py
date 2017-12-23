03c85868d49dc9013db9f926e5fda6df93a7c56f
gmaclinuxer <gmaclinuxer@github.com>
2017-12-23 17:22:54 +0800

    feature: project refactor export settings to config.py and local_settings.py

--- app.py
+++ app.py
@@ -6,29 +6,20 @@ from flask import request, url_for, session, escape
 from flask import redirect, abort, make_response, render_template
 from werkzeug.utils import secure_filename
 
-
-def debug_request():
-    print request.args, request.form, request.is_json
-
-
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-
 app = Flask(__name__)
 
-# 代码自动重载，500错误时支持在页面调试 Werkzeug
-# app.debug = True
+# load config file from config.py
+app.config.from_object('config')
 
-# os.urandom(24)
-app.secret_key = '{\xe6s\xe4\x9a\x92!\xe1\x80\xc9$DF\x13^\x93\xa8}\xec\x9c\x11=\\\x85'
-
-# 日志配置
+# logger to local file flask.log
 if app.debug is not True:
-
     import logging
     from logging.handlers import RotatingFileHandler
 
-    file_handler = RotatingFileHandler(os.path.join(BASE_DIR, 'flask.log'),
-                                       maxBytes=1024 * 1024 * 100, backupCount=20)
+    file_handler = RotatingFileHandler(
+        os.path.join(app.config.BASE_DIR, 'flask.log'),
+        maxBytes=1024 * 1024 * 100, backupCount=20
+    )
     file_handler.setLevel(logging.INFO)
     formatter = logging.Formatter("[ %(asctime)s ][ %(levelname)s ][ %(name)s ]: %(message)s")
     file_handler.setFormatter(formatter)
@@ -37,30 +28,15 @@ if app.debug is not True:
 
 @app.route('/')
 def index():
-    # debug_request()
     app.logger.info('render index template.')
     return render_template('index.html', **{
         'title': u"标题",
         'name': 'pitou',
     })
 
-    # if 'username' in session:
-    #     return 'Logged in as %s' % escape(session['username'])
-    # return 'You are not logged in'
-
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    # if request.method == 'POST':
-    #     session['username'] = request.form['username']
-    #     return redirect(url_for('index'))
-    #
-    # return '''
-    #     <form action="" method="post">
-    #         <p><input type=text name=username>
-    #         <p><input type=submit value=Login>
-    #     </form>
-    # '''
     error = None
     if request.method == 'POST':
         if request.form['username'] != 'admin' or \
@@ -81,11 +57,6 @@ def logout():
 # 访问 /about/ 404
 @app.route('/about')
 def about():
-    # 1/0
-    # abort(401)
-    # abort(501)
-    # abort(500)
-    # abort(403)
     return redirect(url_for('index'))
 
 
@@ -109,10 +80,9 @@ def user(username):
 
 @app.route('/post/<int:post_id>', methods=['GET', 'POST'])
 def post(post_id):
-    debug_request()
     f = request.files['the_file']
     filename = '%s_upload' % secure_filename(f.filename)
-    f.save(os.path.join(BASE_DIR, filename))
+    f.save(os.path.join(app.config.BASE_DIR, filename))
 
     return 'post_id: %s' % post_id
 
@@ -130,10 +100,12 @@ def cookies():
 
 if __name__ == '__main__':
     print 'start app: %s' % __name__
+
     with app.test_request_context():
         print url_for('index')
         print url_for('about')
         print url_for('user', username='123')
         print url_for('static', filename='css/index.css')
         print url_for('static', filename='js/index.js')
-    app.run(debug=True)
+
+    app.run(host='0.0.0.0', port=5000, debug=app.debug)
---
+++ config.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+"""
+    #: Default configuration parameters.
+    default_config = ImmutableDict({
+        'DEBUG':                                get_debug_flag(default=False),
+        'TESTING':                              False,
+        'PROPAGATE_EXCEPTIONS':                 None,
+        'PRESERVE_CONTEXT_ON_EXCEPTION':        None,
+        'SECRET_KEY':                           None,
+        'PERMANENT_SESSION_LIFETIME':           timedelta(days=31),
+        'USE_X_SENDFILE':                       False,
+        'LOGGER_NAME':                          None,
+        'LOGGER_HANDLER_POLICY':               'always',
+        'SERVER_NAME':                          None,
+        'APPLICATION_ROOT':                     None,
+        'SESSION_COOKIE_NAME':                  'session',
+        'SESSION_COOKIE_DOMAIN':                None,
+        'SESSION_COOKIE_PATH':                  None,
+        'SESSION_COOKIE_HTTPONLY':              True,
+        'SESSION_COOKIE_SECURE':                False,
+        'SESSION_REFRESH_EACH_REQUEST':         True,
+        'MAX_CONTENT_LENGTH':                   None,
+        'SEND_FILE_MAX_AGE_DEFAULT':            timedelta(hours=12),
+        'TRAP_BAD_REQUEST_ERRORS':              False,
+        'TRAP_HTTP_EXCEPTIONS':                 False,
+        'EXPLAIN_TEMPLATE_LOADING':             False,
+        'PREFERRED_URL_SCHEME':                 'http',
+        'JSON_AS_ASCII':                        True,
+        'JSON_SORT_KEYS':                       True,
+        'JSONIFY_PRETTYPRINT_REGULAR':          True,
+        'JSONIFY_MIMETYPE':                     'application/json',
+        'TEMPLATES_AUTO_RELOAD':                None,
+    })
+"""
+
+import os
+
+# code reload/debug online
+DEBUG = True
+SECRET_KEY = os.urandom(24)
+
+# root directory
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+# import settings from local
+try:
+    from local_settings import *
+except:
+    pass
