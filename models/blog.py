adb7fb7e1c4e04ed06bd267977da7b101d5457cd
gmaclinuxer <gmaclinuxer@github.com>
2017-12-24 11:58:31 +0800

    feature: add extra models and load_schema command

--- models/blog.py
+++ models/blog.py
@@ -35,3 +35,18 @@ class Category(db.Model):
 
     def __repr__(self):
         return '<Category %r>' % self.name
+
+
+class Todo(db.Model):
+    __tablename__ = 'todos'
+    id = db.Column('todo_id', db.Integer, primary_key=True)
+    title = db.Column(db.String(60))
+    text = db.Column(db.String)
+    done = db.Column(db.Boolean)
+    pub_date = db.Column(db.DateTime)
+
+    def __init__(self, title, text):
+        self.title = title
+        self.text = text
+        self.done = False
+        self.pub_date = datetime.utcnow()
