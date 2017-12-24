a7f6c8e93a8909fda27e0882219f9755f1079875
gmaclinuxer <gmaclinuxer@github.com>
2017-12-24 21:42:25 +0800

    feature: add flask-admin custom config

--- models/admin.py
+++ models/admin.py
@@ -8,8 +8,21 @@ from models.blog import *
 
 admin = Admin(name='Flask-Admin', template_mode='bootstrap3')
 
+
+class PostModelView(ModelView):
+    can_delete = False  # disable model deletion
+    # can_create = False
+    # can_edit = False
+    can_view_details = True
+
+    page_size = 50  # the number of entries to display on the list view
+    column_exclude_list = ['pub_date', ]
+    column_searchable_list = ['title', 'body']
+    column_filters = ['title']
+
+
 admin.add_view(ModelView(User, db.session))
-admin.add_view(ModelView(Post, db.session))
+admin.add_view(PostModelView(Post, db.session))
 admin.add_view(ModelView(Address, db.session))
 admin.add_view(ModelView(Category, db.session))
 admin.add_view(ModelView(Todo, db.session))
