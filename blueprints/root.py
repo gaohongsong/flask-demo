9a116d1e8259c59540b58ba8fce216eccaed9942
gmaclinuxer <gmaclinuxer@github.com>
2017-12-26 14:35:02 +0800

    integrated: Flask-Celery-Helper test failed

--- blueprints/root.py
+++ blueprints/root.py
@@ -1,11 +1,13 @@
 # -*- coding: utf-8 -*-
 """Public section, including homepage and signup."""
+import base64
 
 from flask import (
     Flask, Blueprint, request, session, g,
     redirect, url_for, abort, render_template,
     flash, current_app, make_response
 )
+from flask_admin.helpers import is_safe_url
 
 from flask_login import login_required, login_user, logout_user
 
@@ -25,10 +27,44 @@ def get_all_comments():
     return comments
 
 
+# get user by id
 @login_manager.user_loader
 def load_user(user_id):
     """Load user by ID."""
     return User.get_by_id(int(user_id))
+
+
+# @login_manager.request_loader
+# def request_loader(request):
+#     """ callback for loading a user from a Flask request.
+#         Always return user(id=1) for test only
+#      """
+#     user = User.get_by_id(1)
+#     return user
+
+@login_manager.request_loader
+def load_user_from_request(request):
+    # first, try to login using the api_key url arg
+    api_key = request.args.get('api_key')
+    if api_key:
+        user = User.query.filter_by(api_key=api_key).first()
+        if user:
+            return user
+
+    # next, try to login using Basic Auth
+    api_key = request.headers.get('Authorization')
+    if api_key:
+        api_key = api_key.replace('Basic ', '', 1)
+        try:
+            api_key = base64.b64decode(api_key)
+        except TypeError:
+            pass
+        user = User.query.filter_by(api_key=api_key).first()
+        if user:
+            return user
+
+    # finally, return None if both methods did not login the user
+    return None
 
 
 @bp.route('/', methods=['GET', 'POST'])
@@ -40,7 +76,11 @@ def home():
         if form.validate_on_submit():
             login_user(form.user)
             flash('You are logged in.', 'success')
+
             redirect_url = request.args.get('next') or url_for('member.members')
+            if not is_safe_url(redirect_url):
+                return abort(400)
+
             return redirect(redirect_url)
         else:
             flash_errors(form)
@@ -54,7 +94,7 @@ def logout():
     """Logout."""
     logout_user()
     flash('You are logged out.', 'info')
-    return redirect(url_for('public.home'))
+    return redirect(url_for('root.home'))
 
 
 @bp.route('/register/', methods=['GET', 'POST'])
